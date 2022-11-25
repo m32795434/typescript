a1e7839b5ec882c1bb6ddbc3dc6004e9ddc5fd63
Manuel Augusto Bravard <manubravard@gmail.com>
2022-11-24 23:10:14 -0300

    some scripts and a little example of interface work

--- fetch-json/index.js
+++ fetch-json/index.js
@@ -1,11 +1,17 @@
 "use strict";
 exports.__esModule = true;
 var axios_1 = require("axios");
-var url = 'https://jsonplaceholder.typicode.com/todos/1';
+var url = 'https://jsonplaceholder.typicode.com/todos/2';
+var axiosResp = axios_1["default"].get(url);
+console.log(axiosResp);
 axios_1["default"].get(url).then(function (response) {
+    console.log(response);
     var todo = response.data;
-    var id = todo.ID;
-    var title = todo.Title;
-    var finished = todo.finished;
-    console.log("\n    The todo with id: ".concat(id, " \n    has a title of ").concat(title, "\n    Is it finished?: ").concat(finished, "\n    "));
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    logTodo(id, title, completed);
 });
+var logTodo = function (id, title, completed) {
+    console.log("\n    The todo with id: ".concat(id, " \n    has a title of ").concat(title, "\n    Is it finished?: ").concat(completed, "\n    "));
+};
